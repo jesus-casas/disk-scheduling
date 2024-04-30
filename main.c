ea44e45b7f961eedf6d325e702234bf6f9e3b3d8
jesus-casas <javiercasas740@gmail.com>
2024-04-29 19:51:50 -0700

    Added user interface and empty functions.

--- main.c
+++ main.c
@@ -1,6 +1,75 @@
 #include <stdio.h>
 
+#define MAX_TRACKS 100
+
+void enter_parameters(int *tracks, int *m, int *starting_track) {
+
+}
+
+int calculate_distance_fifo(int *tracks, int m, int starting_track) {
+    return 0;
+}
+
+int calculate_distance_sstf(int *tracks, int m, int starting_track) {
+    return 0;
+}
+
+int calculate_distance_scan(int *tracks, int m, int starting_track) {
+    return 0;
+}
+
+int calculate_distance_cscan(int *tracks, int m, int starting_track) {
+    return 0;
+}
+
+void sort_tracks(int *tracks, int m) {
+
+}
+
+void print_tracks(int *tracks, int m, int starting_track) {
+
+}
+
+// Main ----------------------------
 int main() {
-    printf("Hello, World!\n");
+    
+    // 
+    int user_input, m = 0, starting_track = 0;
+    int tracks[MAX_TRACKS];
+
+    while (1) {
+        printf("\nDisk Scheduling Menu:\n");
+        printf("1) Enter parameters\n");
+        printf("2) Calculate distance to traverse tracks using FIFO\n");
+        printf("3) Calculate distance to traverse tracks using SSTF\n");
+        printf("4) Calculate distance to traverse tracks using Scan\n");
+        printf("5) Calculate distance to traverse tracks using C-Scan\n");
+        printf("6) Quit program and free memory\n");
+        printf("Enter your choice: ");
+        scanf("%d", &user_input);
+
+        switch (user_input) {
+            case 1:
+                enter_parameters(tracks, &m, &starting_track);
+                break;
+            case 2:
+                printf("FIFO Distance: %d\n", calculate_distance_fifo(tracks, m, starting_track));
+                break;
+            case 3:
+                printf("SSTF Distance: %d\n", calculate_distance_sstf(tracks, m, starting_track));
+                break;
+            case 4:
+                printf("Scan Distance: %d\n", calculate_distance_scan(tracks, m, starting_track));
+                break;
+            case 5:
+                printf("C-Scan Distance: %d\n", calculate_distance_cscan(tracks, m, starting_track));
+                break;
+            case 6:
+                printf("Exiting program...\n");
+                return 0;
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    } // end while
     return 0;
 }
