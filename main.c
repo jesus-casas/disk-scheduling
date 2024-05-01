17929100837493104e39f763c21ef505b6b8846a
jesus-casas <javiercasas740@gmail.com>
2024-05-01 12:22:56 -0700

    Made some fundamental changes to the code. Changed variables to be global. Made all functions. All functions call print_tracks now.

--- main.c
+++ main.c
@@ -1,46 +1,83 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <math.h>
 #define MAX_TRACKS 100
 
-void enter_parameters(int *m) {
+// global variables
+int tracks[MAX_TRACKS];
+int m = 0;
+int starting_track = 0;
+
+// Function declarations
+void enter_parameters();
+void calculate_distance_fifo();
+void calculate_distance_sstf();
+void calculate_distance_scan();
+void calculate_distance_cscan();
+void sort_tracks(int *tracks, int m);
+void print_tracks(int distance);
+
+
+void enter_parameters() {
     printf("Enter size of sequence: ");
-    scanf("%d", m);
+    scanf("%d", &m);
 
 }
 
-int calculate_distance_fifo(int *tracks, int m, int starting_track) {
-    return 0;
+void calculate_distance_fifo() {
+    printf("Enter starting track: ");
+    scanf("%d", &starting_track);
+
+    printf("Enter sequence of tracks to seek: ");
+    for (int i = 0; i < m - 1; i++) {
+        scanf("%d", &tracks[i]);
+    }
+
+    int distance = 0;
+    int current_track = starting_track;
+
+    for (int i = 0; i < m - 1; i++) {
+        distance += abs(tracks[i] - current_track);
+        current_track = tracks[i];
+    }
+
+    print_tracks(distance);
 }
 
-int calculate_distance_sstf(int *tracks, int m, int starting_track) {
-    return 0;
+
+void calculate_distance_sstf() {
+    
 }
 
-int calculate_distance_scan(int *tracks, int m, int starting_track) {
-    return 0;
+void calculate_distance_scan() {
+    
 }
 
-int calculate_distance_cscan(int *tracks, int m, int starting_track) {
-    return 0;
+void calculate_distance_cscan() {
+    
 }
 
-void sort_tracks(int *tracks, int m) {
 
-}
+void print_tracks(int distance) {
+    printf("Traversed sequence: %d ", starting_track);
 
-void print_tracks(int *tracks, int m, int starting_track) {
-
+    for (int i = 0; i < m - 1; i++) {
+        printf("%d ", tracks[i]); // Print each track
+    }
+    printf("\n");
+    printf("The distance of the traversed tracks is: %d\n", distance); // Print the total distance
+    printf("\n");
 }
 
 // Main ----------------------------
 int main() {
     
     // 
-    int user_input, m = 0, starting_track = 0;
-    int tracks[MAX_TRACKS];
+    int user_input;
 
     while (1) {
         printf("\nDisk Scheduling Menu:\n");
+        printf("-----------------------\n");
         printf("1) Enter parameters\n");
         printf("2) Calculate distance to traverse tracks using FIFO\n");
         printf("3) Calculate distance to traverse tracks using SSTF\n");
@@ -52,22 +89,22 @@ int main() {
 
         switch (user_input) {
             case 1:
-                enter_parameters(&m);
+                enter_parameters();
                 break;
             case 2:
-                printf("FIFO Distance: %d\n", calculate_distance_fifo(tracks, m, starting_track));
+                calculate_distance_fifo();
                 break;
             case 3:
-                printf("SSTF Distance: %d\n", calculate_distance_sstf(tracks, m, starting_track));
+                calculate_distance_sstf();
                 break;
             case 4:
-                printf("Scan Distance: %d\n", calculate_distance_scan(tracks, m, starting_track));
+                calculate_distance_scan();
                 break;
             case 5:
-                printf("C-Scan Distance: %d\n", calculate_distance_cscan(tracks, m, starting_track));
+                calculate_distance_cscan();
                 break;
             case 6:
-                printf("Exiting program...\n");
+                printf("Quitting program...\n");
                 return 0;
             default:
                 printf("Invalid choice! Please try again.\n");
