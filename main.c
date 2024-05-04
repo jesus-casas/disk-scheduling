0bebe3cf72cbb7e70cb7e3968cdccd147d56d243
jesus-casas <javiercasas740@gmail.com>
2024-05-03 23:08:12 -0700

    Finished sstf algorithm

--- main.c
+++ main.c
@@ -44,10 +44,48 @@ void calculate_distance_fifo() {
     print_tracks(distance);
 }
 
-
 void calculate_distance_sstf() {
-    
+    printf("Enter starting track: ");
+    scanf("%d", &starting_track); // Get the starting track
+
+    printf("Enter sequence of tracks to seek: ");
+    for (int i = 0; i < m - 1; i++) {
+        scanf("%d", &tracks[i]); // Fill in the track requests
+    }
+
+    int distance = 0;
+    int visited[MAX_TRACKS] = {0}; // Array to mark visited tracks
+    int current_track = starting_track;
+    int count = m - 1;
+
+    // Process each track request
+    while (count > 0) {
+        int nearest = -1;
+        int min_distance = 1000000;
+
+        // Find the nearest unvisited track
+        for (int j = 0; j < m - 1; j++) {
+            if (!visited[j]) {
+                int seek_distance = abs(tracks[j] - current_track);
+                if (seek_distance < min_distance) {
+                    min_distance = seek_distance;
+                    nearest = j;
+                }
+            }
+        }
+
+        if (nearest != -1) { // If a nearest track is found
+            visited[nearest] = 1; // Mark this track as visited
+            distance += min_distance; // Add the distance to the total
+            current_track = tracks[nearest]; // Move to this track
+            count--; // Reduce the number of tracks left to process
+        }
+    }
+
+    print_tracks(distance); // Print the results
 }
+
+
 
 void calculate_distance_scan() {
     
