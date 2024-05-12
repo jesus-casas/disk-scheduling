e7d874c70c5fd409bc7f3dac3facdeb06a6bdaed
jesus-casas <javiercasas740@gmail.com>
2024-05-12 12:04:20 -0700

    Made some fundamental changes to the print method and fixed the distance to print out the proper distance value.

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 #define MAX_TRACKS 100
 
 // global variables
 int tracks[MAX_TRACKS];
+int accessed_tracks[MAX_TRACKS];
 int m = 0;
 int starting_track = 0;
+int INT_MAX = 2147483647;
 
 // Function declarations
 void enter_parameters();
 void calculate_distance_fifo();
 void calculate_distance_sstf();
-void calculate_distance_scan();
+//void calculate_distance_scan();
 void calculate_distance_cscan();
-void sort_tracks(int *tracks, int m);
-void print_tracks(int distance);
-
+void print_tracks(int *accessed_tracks, int num_accessed, int distance);
+void insertion_sort(int arr[], int n);
 
 void enter_parameters() {
     printf("Enter size of sequence: ");
@@ -24,46 +26,49 @@ void enter_parameters() {
 
 }
 
+// *************** Calculate distance function FIFO ***************
 void calculate_distance_fifo() {
     printf("Enter starting track: ");
     scanf("%d", &starting_track);
+    printf("Enter sequence of tracks to seek: ");
+    
+    accessed_tracks[0] = starting_track; // First element is the starting track
+    for (int i = 0; i < m - 1; i++) {
+        scanf("%d", &tracks[i]);
+        accessed_tracks[i + 1] = tracks[i]; // Copy to accessed_tracks for consistency, starting from next index
+    }
 
+    int distance = 0;
+    int current_track = starting_track;
+
+    for (int i = 1; i < m; i++) { // Start from 1 because the first track (starting track) doesn't involve movement
+        distance += abs(accessed_tracks[i] - current_track);
+        current_track = accessed_tracks[i];
+    }
+
+    print_tracks(accessed_tracks, m, distance); // Now we pass 'm' as the number of accessed tracks
+}
+
+
+// *************** Calculate distance function SSTF ***************
+void calculate_distance_sstf() {
+    printf("Enter starting track: ");
+    scanf("%d", &starting_track);
     printf("Enter sequence of tracks to seek: ");
     for (int i = 0; i < m - 1; i++) {
         scanf("%d", &tracks[i]);
     }
 
     int distance = 0;
+    int visited[MAX_TRACKS] = {0};  // Initializes the visited array to zero
     int current_track = starting_track;
+    int accessed_index = 0;
+    accessed_tracks[accessed_index++] = starting_track; // Add starting track initially
 
-    for (int i = 0; i < m - 1; i++) {
-        distance += abs(tracks[i] - current_track);
-        current_track = tracks[i];
-    }
-
-    print_tracks(distance);
-}
-
-void calculate_distance_sstf() {
-    printf("Enter starting track: ");
-    scanf("%d", &starting_track); // Get the starting track
-
-    printf("Enter sequence of tracks to seek: ");
-    for (int i = 0; i < m - 1; i++) {
-        scanf("%d", &tracks[i]); // Fill in the track requests
-    }
-
-    int distance = 0;
-    int visited[MAX_TRACKS] = {0}; // Array to mark visited tracks
-    int current_track = starting_track;
     int count = m - 1;
-
-    // Process each track request
     while (count > 0) {
         int nearest = -1;
-        int min_distance = 1000000;
-
-        // Find the nearest unvisited track
+        int min_distance = INT_MAX;
         for (int j = 0; j < m - 1; j++) {
             if (!visited[j]) {
                 int seek_distance = abs(tracks[j] - current_track);
@@ -74,36 +79,38 @@ void calculate_distance_sstf() {
             }
         }
 
-        if (nearest != -1) { // If a nearest track is found
-            visited[nearest] = 1; // Mark this track as visited
-            distance += min_distance; // Add the distance to the total
-            current_track = tracks[nearest]; // Move to this track
-            count--; // Reduce the number of tracks left to process
+        if (nearest != -1) { // Check if a nearest track is found
+            visited[nearest] = 1;
+            distance += min_distance;
+            current_track = tracks[nearest];
+            // Check to prevent adding the starting track twice
+            if (current_track != starting_track || accessed_index == 1) {
+                accessed_tracks[accessed_index++] = current_track;
+            }
+            count--;
+        } else {
+            // If no unvisited track is found, end the loop
+            break;
         }
     }
 
-    print_tracks(distance); // Print the results
+    print_tracks(accessed_tracks, accessed_index, distance);
 }
 
-
-
-void calculate_distance_scan() {
-    
-}
+// *************** Calculate distance function for SCAN **************
 
 void calculate_distance_cscan() {
     
 }
 
-
-void print_tracks(int distance) {
-    printf("Traversed sequence: %d ", starting_track);
-
-    for (int i = 0; i < m - 1; i++) {
-        printf("%d ", tracks[i]); // Print each track
+// *************** Print function ***************
+void print_tracks(int *accessed_tracks, int num_accessed, int distance) {
+    printf("Traversed sequence: ");
+    for (int i = 0; i < num_accessed; i++) {
+        printf("%d ", accessed_tracks[i]);
     }
     printf("\n");
-    printf("The distance of the traversed tracks is: %d\n", distance); // Print the total distance
+    printf("The distance of the traversed tracks is: %d\n", distance);
     printf("\n");
 }
 
@@ -136,7 +143,7 @@ int main() {
                 calculate_distance_sstf();
                 break;
             case 4:
-                calculate_distance_scan();
+                //calculate_distance_scan();
                 break;
             case 5:
                 calculate_distance_cscan();
